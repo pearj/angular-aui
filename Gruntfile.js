9c9dd3bbfe5baae9a3052d7da773278f61398c77
OleksandrB <oleksandr.bezhan@gmail.com>
2014-09-30 17:49:16 +0000

    Make dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        project: {
+            src: 'src'
+        },
         bower: {
             install: {
                 options: {
@@ -15,17 +18,43 @@ module.exports = function(grunt) {
                 options: {
                     port: 8080,
                     base: '.',
-                    keepalive: true
+                    keepalive: false
                 }
+            }
+        },
+        concat: {
+            options: {
+              separator: ';',
+              stripBanners: true,
+              banner: 
+                '/*! \n' + 
+                '* <%= pkg.name %>\n' + 
+                '* Version - <%= pkg.version %>\n' + 
+                '* Built - <%= grunt.template.today("yyyy-mm-dd") %>\n' + 
+                '* Author - <%= pkg.author %>\n' + 
+                '* url - <%= pkg.repository.url %>\n' + 
+                '*/\n',
+            },
+            dist: {
+              src: ['src/angular-aui.js', 'src/**.js'],
+              dest: 'dist/angular-aui.js',
+            },
+          },
+        watch: {
+            concat: {
+                files: '<%= project.src %>/*.js',
+                tasks: ['concat']
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-release');
 
     grunt.registerTask('install', ['bower:install'])
-    grunt.registerTask('default', ['connect']);
+    grunt.registerTask('default', ['connect', 'watch']);
 
 };
---
+++ dist/angular-aui.js
@@ -0,0 +1,68 @@
+/*! 
+* angular-aui
+* Version - 0.0.1
+* Built - 2014-09-30
+* Author - oleksandr.bezhan@gmail.com
+* url - https://github.com/OleksandrBezhan/angular-aui.git
+*/
+'strict mode';
+
+angular.module('angular-aui', ['angular-aui-directives']);;angular.module('angular-aui-directives', [])
+.directive('auiDatePicker', [function() {
+    function link(scope, element, attrs, ngModelCtrl) {
+        AJS.$(element).datePicker({
+          overrideBrowserDefault: true,
+          onSelect: function(newValue) {
+            scope.$apply(function() {
+            // model doesn't get updates  when `overrideBrowserDefault` = true
+                ngModelCtrl.$setViewValue(newValue);
+            });
+          }
+        });
+    }
+    return {
+        require: 'ngModel',
+        link: link
+    };
+}])
+.directive('auiInlineDialog', ['$compile', function($compile) {
+    var dialogContent;
+    function link(scope, element, attrs) {
+        var dialog = AJS.InlineDialog(AJS.$(element), 1,
+          function(content, trigger, showPopup) {
+            content.html(dialogContent);
+            showPopup();
+            return false;
+        }, scope.$eval(attrs.options));
+        scope.$hide = dialog.hide.bind(dialog);
+        scope.$refresh = dialog.refresh.bind(dialog);
+        scope.$show = dialog.show.bind(dialog);
+    }
+    function controller($scope) {
+        this.drawContent = function(html) {
+            dialogContent = $compile(html)($scope);
+        };
+    }
+    return {
+        controller: controller,
+        link: link
+    };
+}])
+.directive('auiInlineDialogContent', ['$timeout', function($timeout) {
+    function link(scope, element, attrs, inlineDialogCtrl) {
+        inlineDialogCtrl.drawContent(element[0].innerHTML);
+        element.css('display', 'none');
+    }
+    return {
+        require: '^auiInlineDialog',
+        link: link
+    };
+}])
+.directive('auiSelect2', [function() {
+    function link($scope, element, attrs) {
+        AJS.$(element).auiSelect2();
+    }
+    return {
+        link: link
+    };
+}]);
